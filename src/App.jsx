393d40aeb70d13a15ca0bd9eb4437c9593e69198
Rafal <rf.zajac@gmail.com>
2023-09-07 16:13:20 +0200

    added cards and flippers

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,43 @@
 import { useEffect, useState } from 'react';
 import NavSection from './components/NavSection'
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CharacterCard from './components/CharacterCard';
+
+
 
 
 
 function App() {
 
   const [searchVal, setsearchVal] = useState("");
+  const [fetchData, setfetchData] = useState([])
   
-  console.log("Search val--->", searchVal)
-  
-
-  // // * Fetch data from API
-  // const url = "https://rickandmortyapi.com/api/character";
-
-  // const fetchCharacters = async () => {
-  //   try {
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log("Error ---->", error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchCharacters()
-  // }, [])
   
 
+  // * Fetch data from API
+  const url = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setfetchData(data);
+    } catch (error) {
+      console.log("Error ---->", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters()
+  }, [])
+
+  
   return (
     <>
       <NavSection setsearchVal={setsearchVal} />
-      <div>This is main section</div>
+      <CharacterCard fetchData={fetchData} />      
     </>
   )
 }
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import '../style/imageGallery.css';
+import Button from 'react-bootstrap/Button';
 
-function CharacterCard() {
+function CharacterCard({fetchData}) {
+  
+  const characters = fetchData.results;
+  
   return (
-    <div>CharacterCard</div>
+    <>
+      <Row className='justify-content-md-center'id='row'>
+        <Col xs={10}>
+               <Row className="g-3" id='gallery' xs={5}>
+            {characters && characters.map((character, index) => (
+              <Col key={index}>
+                <div className="flip-box">
+                  <div className='flip-box-inner'>
+                    <div className="flip-box-front">
+                       <Card className='card'>
+                         <Card.Img src={character.image}></Card.Img>
+                      </Card>
+                    </div>
+                    <div className='flip-box-back'> 
+                      <h2>{character.name}</h2>
+                      <Button id='learn-more'>Learn more</Button>
+                    </div>
+                  </div>
+                </div>
+                
+                            
+              </Col>
+        ))}
+      </Row>
+         
+        </Col>
+      </Row>
+
+      
+  
+    </>
   )
 }
 
